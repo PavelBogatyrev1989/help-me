a4b62029445e3e4e4d757a82f4613ac018451e31
Pavel Bogatyrev <bogatyrevpavel1989@gmail.com>
2018-03-14 05:42:48 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function count(s, pairs) {
 
   let N = 1;
   let answer = 0;
-
+  let flag;
   for (let i = 0; i < pairs.length; i++) {
     N = N * (pairs[i][0] ** pairs[i][1]);
     if (N > 10000000)
@@ -11,14 +11,11 @@ module.exports = function count(s, pairs) {
 
   for (let k = 0; k < N; k++) {
     for (let j = 0; j < s.length; j++) {
-      let gcdVal = gcd(k + j, N) == 1 ? 1 : 0;
-      if (gcdVal == s[j])  {
-        answer++;
-      } else if (gcdVal != s[j]) {
-        break;
-      }
-
+    let gcdVal = gcd(k + j, N) == 1 ? 1 : 0;
+    flag = gcdVal == s[j] ? true : false;
+    if(!flag) break;
     }
+    if(flag)  answer++;
   }
   return answer % 1000000007;
 }
